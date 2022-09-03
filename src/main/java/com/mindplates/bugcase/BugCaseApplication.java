16ba793beaf1c69046cd99bbd6ca3a4714f26fc0
kevin.12 <kevin.12@kakaocorp.com>
2022-09-04 01:06:49 +0900

    i

---
+++ src/main/java/com/mindplates/bugcase/BugCaseApplication.java
@@ -0,0 +1,13 @@
+package com.mindplates.bugcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mindplates/bugcase/BugCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindplates.bugcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
