5aeedee58a8c27bbf15525d139161986829d49aa
kevin.12 <kevin.12@kakaocorp.com>
2022-12-26 16:01:53 +0900

    스페이스 조회 디자인 변경

--- app/src/components/Title/Title.jsx
+++ app/src/components/Title/Title.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Title.scss';
 
-function Title({ className, children, type, control, border }) {
+function Title({ className, children, type, control, border, paddingBottom }) {
   return (
-    <div className={`title-wrapper ${type} ${className} ${border ? 'border' : ''}`}>
+    <div className={`title-wrapper ${type} ${className} ${border ? 'border' : ''} ${paddingBottom ? 'padding-bottom' : ''}`}>
       <div className="title-content">
         <div className="title-text">{children}</div>
         {control && <div className="control">{control}</div>}
@@ -20,6 +20,7 @@ Title.defaultProps = {
   type: 'h1',
   control: null,
   border: true,
+  paddingBottom: true,
 };
 
 Title.propTypes = {
@@ -28,6 +29,7 @@ Title.propTypes = {
   type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5']),
   control: PropTypes.node,
   border: PropTypes.bool,
+  paddingBottom: PropTypes.bool,
 };
 
 export default Title;
