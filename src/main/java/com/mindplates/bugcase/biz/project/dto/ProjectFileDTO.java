58ecb6a3f9d2928caa86bb6ebef2fcd94ca0908e
kevin.12 <kevin.12@kakaocorp.com>
2023-05-17 23:26:12 +0900

    -  프로젝트 파일 조회 성능 개선

--- src/main/java/com/mindplates/bugcase/biz/project/dto/ProjectFileDTO.java
+++ src/main/java/com/mindplates/bugcase/biz/project/dto/ProjectFileDTO.java
@@ -1,5 +1,6 @@
 package com.mindplates.bugcase.biz.project.dto;
 
+import com.mindplates.bugcase.biz.project.entity.ProjectFile;
 import com.mindplates.bugcase.common.code.FileSourceTypeCode;
 import com.mindplates.bugcase.common.dto.CommonDTO;
 import lombok.AllArgsConstructor;
@@ -22,4 +23,16 @@ public class ProjectFileDTO extends CommonDTO {
     private String uuid;
     private FileSourceTypeCode fileSourceType;
     private Long fileSourceId;
+
+    public ProjectFileDTO(ProjectFile projectFile) {
+        this.id = projectFile.getId();
+        this.name = projectFile.getName();
+        this.project = ProjectDTO.builder().id(projectFile.getProject().getId()).build();
+        this.type = projectFile.getType();
+        this.path = projectFile.getPath();
+        this.size = projectFile.getSize();
+        this.uuid = projectFile.getUuid();
+        this.fileSourceType = projectFile.getFileSourceType();
+        this.fileSourceId = projectFile.getFileSourceId();
+    }
 }
