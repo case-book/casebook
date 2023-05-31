56494b8d8015c5a0ebd10217823d8bdfc1386da9
kevin.12 <kevin.12@kakaocorp.com>
2023-05-31 23:58:03 +0900

    - 대시보드 페이지 수정

--- app/src/pages/spaces/projects/ProjectDashBoardPage.jsx
+++ app/src/pages/spaces/projects/ProjectDashBoardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardContent, CardHeader, EmptyContent, Page, PageContent, PageTitle, PieChart, Radio, SeqId } from '@/components';
+import { EmptyContent, Page, PageContent, PageTitle, PieChart, Radio, SeqId, Title } from '@/components';
 import { useTranslation } from 'react-i18next';
 
 import TestrunService from '@/services/TestrunService';
@@ -10,6 +10,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import moment from 'moment';
 import dateUtil from '@/utils/dateUtil';
 import ReactTooltip from 'react-tooltip';
+import ProjectService from '@/services/ProjectService';
 
 function ProjectDashBoardPage() {
   const { t } = useTranslation();
@@ -21,6 +22,7 @@ function ProjectDashBoardPage() {
     { key: '12', value: t('@개월', { month: 12 }) },
   ]);
   const [period, setPeriod] = useState('1');
+  const [project, setProject] = useState(null);
   const [periodRange, setPeriodRange] = useState({});
   const [testruns, setTestruns] = useState([]);
   const [testrunHistories, setTestrunHistories] = useState([]);
@@ -65,6 +67,10 @@ function ProjectDashBoardPage() {
     TestrunService.selectUserAssignedTestrunList(spaceCode, projectId, list => {
       setUserAssignedTestruns(list);
     });
+
+    ProjectService.selectProjectInfo(spaceCode, projectId, info => {
+      setProject(info);
+    });
   }, [spaceCode, projectId]);
 
   useEffect(() => {
@@ -85,8 +91,34 @@ function ProjectDashBoardPage() {
   }, [spaceCode, projectId, period]);
 
   return (
-    <Page className="project-overview-info-page-wrapper" list>
+    <Page className="project-overview-info-page-wrapper">
       <PageTitle
+        breadcrumbs={[
+          {
+            to: '/',
+            text: t('HOME'),
+          },
+          {
+            to: '/',
+            text: t('스페이스 목록'),
+          },
+          {
+            to: `/spaces/${spaceCode}/info`,
+            text: spaceCode,
+          },
+          {
+            to: `/spaces/${spaceCode}/projects`,
+            text: t('프로젝트 목록'),
+          },
+          {
+            to: `/spaces/${spaceCode}/projects/${projectId}/info`,
+            text: project?.name,
+          },
+          {
+            to: `/spaces/${spaceCode}/projects/${projectId}`,
+            text: t('대시보드'),
+          },
+        ]}
         onListClick={() => {
           navigate(`/spaces/${spaceCode}/projects`);
         }}
@@ -94,9 +126,11 @@ function ProjectDashBoardPage() {
         {t('대시보드')}
       </PageTitle>
       <PageContent flex>
-        <Card className="testruning-info">
-          <CardHeader className="card-header">{t('진행 중인 테스트런')}</CardHeader>
-          <CardContent scroll horizontal className={`card-content ${testruns?.length < 1 ? 'empty' : ''}`}>
+        <Title border={false} marginBottom={false}>
+          {t('진행 중인 테스트런')}
+        </Title>
+        <div className="scroll-content">
+          <div>
             {testruns.length < 1 && <EmptyContent className="empty-content">{t('진행 중인 테스트런이 없습니다.')}</EmptyContent>}
             {testruns.length > 0 && (
               <ul>
@@ -177,11 +211,13 @@ function ProjectDashBoardPage() {
                 })}
               </ul>
             )}
-          </CardContent>
-        </Card>
-        <Card className="my-testrun-info">
-          <CardHeader className="card-header">{t('내가 진행해야할 테스트')}</CardHeader>
-          <CardContent scroll horizontal className={`card-content my-testrun-content  ${userAssignedTestruns?.length < 1 ? 'empty' : ''}`}>
+          </div>
+        </div>
+        <Title border={false} marginBottom={false}>
+          {t('내가 진행해야할 테스트')}
+        </Title>
+        <div className="scroll-content my-testrun-content">
+          <div>
             {userAssignedTestruns.length < 1 && <EmptyContent className="empty-content">{t('할당된 테스트케이스가 없습니다.')}</EmptyContent>}
             {userAssignedTestruns.length > 0 && (
               <ul>
@@ -279,115 +315,112 @@ function ProjectDashBoardPage() {
                 })}
               </ul>
             )}
-          </CardContent>
-        </Card>
-        <Card className="testrun-history">
-          <CardHeader className="card-header">
-            <div>
-              <div className="text">{t('테스트런 히스토리')}</div>
-              <div>
-                {periods.map(d => {
-                  return (
-                    <Radio
-                      key={d.key}
-                      size="sm"
-                      value={d.key}
-                      type="default"
-                      checked={period === d.key}
-                      onChange={val => {
-                        setPeriod(val);
-                      }}
-                      label={d.value}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </CardHeader>
-          <CardContent className="testrun-history-content">
-            <div className="testrun-history-chart">
-              <div className="chart-content">
-                {testrunHistories.length < 1 && <EmptyContent className="empty-content">{t('테스트런 히스토리가 없습니다.')}</EmptyContent>}
-                {testrunHistories.length > 0 && (
-                  <ul>
-                    {testrunHistories
-                      .sort((a, b) => {
-                        return moment(a.startDateTime) - moment(b.startDateTime);
-                      })
-                      .map(d => {
-                        const start = periodRange.start?.valueOf();
-                        const end = periodRange.end?.valueOf();
-                        const totalSpan = end - start;
-                        const currentCloseSpan = moment(d.closedDate || d.endDateTime).valueOf() - moment(d.startDateTime).valueOf();
+          </div>
+        </div>
+        <Title
+          border={false}
+          marginBottom={false}
+          control={periods.map(d => {
+            return (
+              <Radio
+                key={d.key}
+                size="xs"
+                value={d.key}
+                type="inline"
+                checked={period === d.key}
+                onChange={val => {
+                  setPeriod(val);
+                }}
+                label={d.value}
+              />
+            );
+          })}
+        >
+          {t('테스트런 히스토리')}
+        </Title>
+        <div className="testrun-history-content">
+          <div className="testrun-history-chart">
+            <div className="chart-content">
+              {testrunHistories.length < 1 && <EmptyContent className="empty-content">{t('테스트런 히스토리가 없습니다.')}</EmptyContent>}
+              {testrunHistories.length > 0 && (
+                <ul>
+                  {testrunHistories
+                    .sort((a, b) => {
+                      return moment(a.startDateTime) - moment(b.startDateTime);
+                    })
+                    .map(d => {
+                      const start = periodRange.start?.valueOf();
+                      const end = periodRange.end?.valueOf();
+                      const totalSpan = end - start;
+                      const currentCloseSpan = moment(d.closedDate || d.endDateTime).valueOf() - moment(d.startDateTime).valueOf();
 
-                        return (
-                          <li key={d.id}>
+                      return (
+                        <li key={d.id}>
+                          <div
+                            style={{
+                              left: `${((moment(d.startDateTime).valueOf() - start) / totalSpan) * 100}%`,
+                            }}
+                          >
                             <div
+                              className="start-close-line"
                               style={{
-                                left: `${((moment(d.startDateTime).valueOf() - start) / totalSpan) * 100}%`,
+                                width: `${(currentCloseSpan / totalSpan) * 100}%`,
+                              }}
+                              data-tip={`${dateUtil.getDateString(d.startDateTime)}-${dateUtil.getDateString(
+                                moment(d.closedDate || d.endDateTime).valueOf(),
+                                DATE_FORMATS_TYPES.monthsDaysHoursMinutes,
+                              )} [ ${d.passedTestcaseCount} PASSED / ${d.failedTestcaseCount} FAILED ]`}
+                              onClick={() => {
+                                navigate(`/spaces/${spaceCode}/projects/${projectId}/testruns/${d.id}`);
                               }}
                             >
                               <div
-                                className="start-close-line"
+                                className="passed"
                                 style={{
-                                  width: `${(currentCloseSpan / totalSpan) * 100}%`,
+                                  width: `${(d.passedTestcaseCount / d.totalTestcaseCount) * 100}%`,
                                 }}
-                                data-tip={`${dateUtil.getDateString(d.startDateTime)}-${dateUtil.getDateString(
-                                  moment(d.closedDate || d.endDateTime).valueOf(),
-                                  DATE_FORMATS_TYPES.monthsDaysHoursMinutes,
-                                )} [ ${d.passedTestcaseCount} PASSED / ${d.failedTestcaseCount} FAILED ]`}
-                                onClick={() => {
-                                  navigate(`/spaces/${spaceCode}/projects/${projectId}/testruns/${d.id}`);
+                              />
+                              <div
+                                className="failed"
+                                style={{
+                                  width: `${(d.failedTestcaseCount / d.totalTestcaseCount) * 100}%`,
                                 }}
-                              >
-                                <div
-                                  className="passed"
-                                  style={{
-                                    width: `${(d.passedTestcaseCount / d.totalTestcaseCount) * 100}%`,
-                                  }}
-                                />
-                                <div
-                                  className="failed"
-                                  style={{
-                                    width: `${(d.failedTestcaseCount / d.totalTestcaseCount) * 100}%`,
-                                  }}
-                                />
-                              </div>
-                              <div className={`testrun-name ${((moment(d.startDateTime).valueOf() - start) / totalSpan) * 100 > 10 ? 'left' : ''}`}>
-                                <span>{d.name}</span>
-                              </div>
-                              <div className={`testrun-start-date ${((moment(d.startDateTime).valueOf() - start) / totalSpan) * 100 > 10 ? 'left' : ''}`}>
-                                <span>{dateUtil.getDateString(d.startDateTime, DATE_FORMATS_TYPES.days)}</span>
-                              </div>
+                              />
                             </div>
-                          </li>
-                        );
-                      })}
-                  </ul>
-                )}
+                            <div className={`testrun-name ${((moment(d.startDateTime).valueOf() - start) / totalSpan) * 100 > 10 ? 'left' : ''}`}>
+                              <span>{d.name}</span>
+                            </div>
+                            <div className={`testrun-start-date ${((moment(d.startDateTime).valueOf() - start) / totalSpan) * 100 > 10 ? 'left' : ''}`}>
+                              <span>{dateUtil.getDateString(d.startDateTime, DATE_FORMATS_TYPES.days)}</span>
+                            </div>
+                          </div>
+                        </li>
+                      );
+                    })}
+                </ul>
+              )}
+            </div>
+            <div className="chart-axis">
+              <div className="line">
+                <div className="start-line">
+                  <div />
+                </div>
+                <div className="center-line">
+                  <div />
+                </div>
+                <div className="end-line">
+                  <div />
+                </div>
               </div>
-              <div className="chart-axis">
-                <div className="line">
-                  <div className="start-line">
-                    <div />
-                  </div>
-                  <div className="center-line">
-                    <div />
-                  </div>
-                  <div className="end-line">
-                    <div />
-                  </div>
-                </div>
-                <div className="range">
-                  <span>{dateUtil.getDateString(periodRange.start, DATE_FORMATS_TYPES.yearsDays)}</span>
-                </div>
-                <div className="range">
-                  <span>{dateUtil.getDateString(periodRange.end, DATE_FORMATS_TYPES.yearsDays)}</span>
-                </div>
+              <div className="range">
+                <span>{dateUtil.getDateString(periodRange.start, DATE_FORMATS_TYPES.yearsDays)}</span>
+              </div>
+              <div className="range">
+                <span>{dateUtil.getDateString(periodRange.end, DATE_FORMATS_TYPES.yearsDays)}</span>
               </div>
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       </PageContent>
     </Page>
   );
