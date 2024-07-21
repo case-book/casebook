5f10d12a172691e19465ca77e6c63c3e190c97e4
again7536 <hongxing107@gmail.com>
2024-07-21 13:55:21 +0900

    fix: 네비게이터가 작은 사이즈일 때 동작 및 UI 추가

--- app/src/pages/spaces/projects/ProjectTestcaseInfoPage/TestcaseNavigator/TestcaseNavigatorFilter.jsx
+++ app/src/pages/spaces/projects/ProjectTestcaseInfoPage/TestcaseNavigator/TestcaseNavigatorFilter.jsx
@@ -8,6 +8,7 @@ import ReleaseService from '@/services/ReleaseService';
 import useStores from '@/hooks/useStores';
 import { observer } from 'mobx-react';
 import useClickOutside from '@/hooks/useClickOutside';
+import useKeyboard from '@/hooks/useKeyboard';
 import { getFilterIdsFromIdStrings, MAX_ID_RANGE_END } from './testcaseFilterUtils';
 import './TestcaseNavigatorFilter.scss';
 
@@ -16,10 +17,14 @@ function TestcaseNavigatorFilter({ width, testcaseFilter, onChangeTestcaseFilter
   const filterRef = useRef(null);
   const filterNameInputRef = useRef(null); // transition을 활용하기 위해서 uncontrolled input을 사용
   const [isFilterFolded, setIsFilterFolded] = useState(true);
+  const [isFloatingFilterOpened, setFloatingFilterOpened] = useState(false);
   const [filterIdsInput, setFilterIdsInput] = useState('');
   const [filterIdTags, setFilterIdTags] = useState([]);
   const [projectReleases, setProjectReleases] = useState([]);
-  useClickOutside(filterRef, () => {});
+  useClickOutside(filterRef, () => {
+    setFloatingFilterOpened(false);
+  });
+  const { register, unregister, pressed } = useKeyboard('Escape');
 
   const {
     contextStore: { spaceCode, projectId },
@@ -65,8 +70,17 @@ function TestcaseNavigatorFilter({ width, testcaseFilter, onChangeTestcaseFilter
     ReleaseService.selectReleaseList(spaceCode, projectId, setProjectReleases, () => {}, false);
   }, [isFilterFolded, spaceCode, projectId]);
 
+  useEffect(() => {
+    register();
+    return () => unregister();
+  }, [register, unregister]);
+
+  useEffect(() => {
+    if (pressed) setFloatingFilterOpened(false);
+  }, [pressed]);
+
   return (
-    <div ref={filterRef} className={`testcase-filter-group ${width < 280 ? 'floating' : ''}`} style={{ transform: width < 280 ? `translateX(${width}px)` : '' }}>
+    <div className={`testcase-navigator-filter-wrapper ${width < 280 ? 'small-control' : ''} ${width < 230 ? 'smaller-control' : ''}`} ref={filterRef}>
       <Title
         border={false}
         paddingBottom={false}
@@ -74,120 +88,143 @@ function TestcaseNavigatorFilter({ width, testcaseFilter, onChangeTestcaseFilter
         icon={false}
         type="h3"
         control={
-          <div className="testcase-filter-title-control">
-            <Button
-              size="xs"
-              rounded
-              onClick={() => {
-                onChangeTestcaseFilter({ name: '', ids: [], releaseIds: [] });
-                setFilterIdTags([]);
-                filterNameInputRef.current.value = '';
-              }}
-              tip={t('필터 리셋')}
-              color={isFiltered ? 'primary' : 'white'}
-              disabled={!isFiltered}
-            >
-              <i className={`fa-solid ${isFiltered ? 'fa-filter-circle-xmark' : 'fa-filter'}`} />
-            </Button>
-            <Button size="xs" rounded tip={t('필터 확장/축소')} onClick={() => setIsFilterFolded(prev => !prev)}>
-              <i className={`fa-solid fa-${isFilterFolded ? 'chevron-down' : 'chevron-up'}`} />
-            </Button>
-          </div>
+          width < 280 ? (
+            <div>
+              <Button
+                size="xs"
+                rounded
+                onClick={() => {
+                  onChangeTestcaseFilter({ name: '', ids: [], releaseIds: [] });
+                  setFilterIdTags([]);
+                  filterNameInputRef.current.value = '';
+                }}
+                tip={t('필터 리셋')}
+                color={isFiltered ? 'primary' : 'white'}
+                disabled={!isFiltered}
+              >
+                <i className={`fa-solid ${isFiltered ? 'fa-filter-circle-xmark' : 'fa-filter'}`} />
+              </Button>
+              <Button size="xs" rounded onClick={() => setFloatingFilterOpened(prev => !prev)} tip={t(isFloatingFilterOpened ? '필터 닫기' : '필터 열기')}>
+                <i className={`fa-solid ${isFloatingFilterOpened ? 'fa-chevron-left' : 'fa-chevron-right'}`} />
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <Button
+                size="xs"
+                rounded
+                onClick={() => {
+                  onChangeTestcaseFilter({ name: '', ids: [], releaseIds: [] });
+                  setFilterIdTags([]);
+                  filterNameInputRef.current.value = '';
+                }}
+                tip={t('필터 리셋')}
+                color={isFiltered ? 'primary' : 'white'}
+                disabled={!isFiltered}
+              >
+                <i className={`fa-solid ${isFiltered ? 'fa-filter-circle-xmark' : 'fa-filter'}`} />
+              </Button>
+              <Button size="xs" rounded tip={t('필터 확장/축소')} onClick={() => setIsFilterFolded(prev => !prev)}>
+                <i className={`fa-solid fa-${isFilterFolded ? 'chevron-down' : 'chevron-up'}`} />
+              </Button>
+            </div>
+          )
         }
       >
-        {t('테스트케이스 필터')}
+        {width < 230 ? t('필터') : t('테스트케이스 필터')}
       </Title>
-
-      <div className="filter-input-group">
-        <Label minWidth="70px">{t('이름')}</Label>
-        <Input
-          onRef={node => {
-            filterNameInputRef.current = node;
-          }}
-          value={null}
-          size="sm"
-          placeholder={t('테스트케이스 이름')}
-          onChange={value => {
-            onChangeTestcaseFilter({ ...testcaseFilter, name: value });
-          }}
-        />
-      </div>
-      {!isFilterFolded && (
-        <div className="foldable-filter">
-          <div className="filter-input-group">
-            <Label minWidth="70px">{t('TC 번호')}</Label>
-            <Input
-              size="sm"
-              value={filterIdsInput}
-              placeholder={t('숫자 또는 범위 (ex. 1-10) 입력')}
-              onChange={v => setFilterIdsInput(v)}
-              onKeyDown={e => {
-                if (e.key !== 'Enter') return;
-                onAddFilterIds();
-              }}
-            />
-            <Button size="sm" onClick={() => onAddFilterIds()}>
-              +
-            </Button>
-          </div>
-          <div className="testcase-filter-ids-list">
-            {filterIdTags.map(id => {
-              return (
-                <Tag
-                  key={id}
-                  border
-                  size="sm"
-                  onRemove={() => {
-                    const nextFilterIdStrings = filterIdTags.filter(d => d !== id);
-                    const nextFilterIds = getFilterIdsFromIdStrings(nextFilterIdStrings);
-                    if (!nextFilterIds) {
-                      dialogUtil.setMessage(
-                        MESSAGE_CATEGORY.WARNING,
-                        t('입력 오류'),
-                        t('잘못된 입력입니다. 번호는 1 보다 작거나 {{max}}보다 클 수 없고, 범위 시작은 범위 끝보다 작아야 합니다.', { max: MAX_ID_RANGE_END }),
-                      );
-                      return;
-                    }
-                    setFilterIdTags(nextFilterIdStrings);
-                    onChangeTestcaseFilter({ ...testcaseFilter, ids: nextFilterIds });
-                  }}
-                  text={id}
-                >
-                  {id}
-                </Tag>
-              );
-            })}
-          </div>
-          <div className="filter-input-group">
-            <Label minWidth="70px">{t('릴리스')}</Label>
-            <Selector
-              size="sm"
-              items={filterReleaseIdsOptions}
-              onChange={v => {
-                if (testcaseFilter.releaseIds.includes(v)) return;
-                onChangeTestcaseFilter({ ...testcaseFilter, releaseIds: [...testcaseFilter.releaseIds, v] });
-              }}
-            />
-          </div>
-          <div className="testcase-filter-release-ids-list">
-            {testcaseFilter.releaseIds.map(releaseId => {
-              return (
-                <Tag
-                  key={releaseId}
-                  border
-                  size="sm"
-                  onRemove={() => {
-                    onChangeTestcaseFilter({ ...testcaseFilter, releaseIds: testcaseFilter.releaseIds.filter(d => d !== releaseId) });
-                  }}
-                  text={releaseId}
-                >
-                  {projectReleaseMap.get(releaseId)}
-                </Tag>
-              );
-            })}
-          </div>
+      <div className={`filter-input-wrapper ${isFloatingFilterOpened ? 'floating' : ''}`} style={{ transform: `translateX(${width < 280 ? width : 0}px)` }}>
+        <div className="filter-input-group">
+          <Label minWidth="70px">{t('이름')}</Label>
+          <Input
+            onRef={node => {
+              filterNameInputRef.current = node;
+            }}
+            value={null}
+            size="sm"
+            placeholder={t('테스트케이스 이름')}
+            onChange={value => {
+              onChangeTestcaseFilter({ ...testcaseFilter, name: value });
+            }}
+          />
         </div>
-      )}
+        {(isFloatingFilterOpened || (!isFilterFolded && width > 280)) && (
+          <div className="foldable-filter">
+            <div className="filter-input-group">
+              <Label minWidth="70px">{t('TC 번호')}</Label>
+              <Input
+                size="sm"
+                value={filterIdsInput}
+                placeholder={t('숫자 또는 범위 (ex. 1-10) 입력')}
+                onChange={v => setFilterIdsInput(v)}
+                onKeyDown={e => {
+                  if (e.key !== 'Enter') return;
+                  onAddFilterIds();
+                }}
+              />
+              <Button size="sm" onClick={() => onAddFilterIds()}>
+                +
+              </Button>
+            </div>
+            <div className="testcase-filter-ids-list">
+              {filterIdTags.map(id => {
+                return (
+                  <Tag
+                    key={id}
+                    border
+                    size="sm"
+                    onRemove={() => {
+                      const nextFilterIdStrings = filterIdTags.filter(d => d !== id);
+                      const nextFilterIds = getFilterIdsFromIdStrings(nextFilterIdStrings);
+                      if (!nextFilterIds) {
+                        dialogUtil.setMessage(
+                          MESSAGE_CATEGORY.WARNING,
+                          t('입력 오류'),
+                          t('잘못된 입력입니다. 번호는 1 보다 작거나 {{max}}보다 클 수 없고, 범위 시작은 범위 끝보다 작아야 합니다.', { max: MAX_ID_RANGE_END }),
+                        );
+                        return;
+                      }
+                      setFilterIdTags(nextFilterIdStrings);
+                      onChangeTestcaseFilter({ ...testcaseFilter, ids: nextFilterIds });
+                    }}
+                    text={id}
+                  >
+                    {id}
+                  </Tag>
+                );
+              })}
+            </div>
+            <div className="filter-input-group">
+              <Label minWidth="70px">{t('릴리스')}</Label>
+              <Selector
+                size="sm"
+                items={filterReleaseIdsOptions}
+                onChange={v => {
+                  if (testcaseFilter.releaseIds.includes(v)) return;
+                  onChangeTestcaseFilter({ ...testcaseFilter, releaseIds: [...testcaseFilter.releaseIds, v] });
+                }}
+              />
+            </div>
+            <div className="testcase-filter-release-ids-list">
+              {testcaseFilter.releaseIds.map(releaseId => {
+                return (
+                  <Tag
+                    key={releaseId}
+                    border
+                    size="sm"
+                    onRemove={() => {
+                      onChangeTestcaseFilter({ ...testcaseFilter, releaseIds: testcaseFilter.releaseIds.filter(d => d !== releaseId) });
+                    }}
+                    text={releaseId}
+                  >
+                    {projectReleaseMap.get(releaseId)}
+                  </Tag>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
